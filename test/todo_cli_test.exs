a1f8a50cf345ef1f8e6bb152f508102e72b2d209
Andrew Babcock <ababcock013@outlook.com>
2022-06-29 10:33:57 -0500

    Added test for List crud

--- test/todo_cli_test.exs
+++ test/todo_cli_test.exs
@@ -16,4 +16,33 @@ defmodule TodoCliTest do
     new_list = TodoCli.Repo.get!(List, list.id)
     assert new_list.title == "My new list"
   end
+
+  test "retrieve a list by name" do
+    cs = TodoCli.List.changeset(%List{}, %{title: "My other new list"})
+    TodoCli.Repo.insert!(cs)
+    list = TodoCli.Repo.get_by!(List, title: "My other new list")
+    assert list.title == "My other new list"
+  end
+
+  test "Update list name" do
+    list = TodoCli.Repo.insert!(%List{title: "My poorly named list"})
+    bad_list = TodoCli.Repo.get!(List, list.id)
+    IO.inspect bad_list
+    from(l in List, where: l.id == ^list.id, update: [set: [title: "Better title"]])
+    |> TodoCli.Repo.update_all([])
+    new_list = TodoCli.Repo.get!(List, list.id)
+    assert new_list.id == list.id
+  end
+
+  test "delete list by name" do
+    list = TodoCli.Repo.insert!(%List{title: "House chores"})
+    result = case TodoCli.Repo.delete(list) do
+      {:ok, struct}  ->
+      "Deleted successfully"
+
+      {:error, _reason} ->
+      "Error"
+    end
+    assert result == "Deleted successfully"
+  end
 end
